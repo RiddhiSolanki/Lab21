b66dc85aae7ae348c8405cddcfa29acbcee56abb
RiddhiSolanki <riddhisolanki91@gmail.com>
2016-09-27 15:44:56 -0400

    Lab 21 09/27/2016

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21.Models;
+
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Index(Response getResponse)
+        {
+            return View("Response", getResponse);
+        }
+    }
+}
---
+++ Lab21/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class Response
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public string Email     { get; set; }
+        public string Phone     { get; set; }
+        public String Address   { get; set; }
+        public bool? Gender    { get; set; }
+        public bool? Agree      { get; set; }                  //? allow null
+    }
+}
